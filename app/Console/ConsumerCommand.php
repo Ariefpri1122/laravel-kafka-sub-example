0ad963ef95ce5d9ea8dced59ed644f2c253c6b2f
Ariefpri1122 <36946700+Ariefpri1122@users.noreply.github.com>
2023-03-10 07:25:43 +0700

    Update ConsumerCommand.php

--- app/Console/ConsumerCommand.php
+++ app/Console/ConsumerCommand.php
@@ -34,9 +34,9 @@ class ConsumerCommand extends Command
     {
         $consumer = new KafkaConsumer($this->getConfig());
 
-        // Subscribe to topic 'inventories'
-        // Microservice 1 will push to 'inventories' topic
-        $consumer->subscribe(['inventories']);
+        // Subscribe to topic 'nama_client'
+        // Microservice 1 will push to 'nama_client' topic
+        $consumer->subscribe(['nama_client']);
 
         while (true) {
             $message = $consumer->consume(120*1000);
@@ -73,8 +73,8 @@ class ConsumerCommand extends Command
 
         // Lets update the stats
         Stat::updateOrCreate(
-            ['inventory_id' => $message->body->id],
-            ['make' => $message->body->make, 'model' => $message->body->model]
+            ['name' => $message->body->name],
+            ['title' => $message->body->title, 'last_name' => $message->body->last_name, 'address' => $message->body->address, 'zip_code' => $message->body->zip_code, 'city' => $message->body->city, 'state' => $message->body->state, 'email' => $message->body->email]
         );
     }
 
@@ -109,7 +109,7 @@ class ConsumerCommand extends Command
         $conf->set('group.id', 'myConsumerGroup');
 
         // Initial list of Kafka brokers
-        $conf->set('metadata.broker.list', env('KAFKA_BROKERS', 'kafka:9092'));
+        $conf->set('metadata.broker.list', env('KAFKA_BROKERS', '192.168.3.154:9092'));
 
         // Set where to start consuming messages when there is no initial offset in
         // offset store or the desired offset is out of range.
